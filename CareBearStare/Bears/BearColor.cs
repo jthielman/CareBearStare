353ec85e794662feadd697285199801bb91337be
feven90 <f.mulugeta90@gmail.com>
2020-03-17 21:01:22 -0500

    Add project files.

---
+++ CareBearStare/Bears/BearColor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareBearStare.Bears
+{
+    enum BearColor
+    {
+        Yellow,
+        Pink,
+        White,
+        Aqua,
+        Mauve,
+        Purple,
+        Gold,
+        Blue,
+        Brown
+
+    }
+}
---
+++ CareBearStare/Bears/CareBearBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareBearStare.Bears
+{
+    abstract class CareBearBase
+    {
+        public string Name { get; protected set; }
+        public BearColor Color { get; protected set; }
+        public string BellyBadge { get; protected set; }
+
+        public abstract void Stare(); // "abstract" means it has to be defined (using "override") when instantiated
+
+        public void Care(string humanToCareFor) // can't be changed
+        {
+            Console.WriteLine($"{Name} cares deeply for {humanToCareFor}.");
+        }
+
+        public virtual void Hug(CareBearBase careBearToHug) // "virtual" means it can be changed or left as is
+        {
+            Console.WriteLine($"{Name} runs over to {careBearToHug.Name} and squeezes them, pressing their {BellyBadge} and {careBearToHug.BellyBadge} together.");
+        }
+
+    }
+}
